2f049dcc93ca563c4ff50465a6747cbbf9ab67ce
Kap <97058263@st.deltion.nl>
2020-08-27 12:01:35 +0200

    Wat scripts en persoonlijke test scene

---
+++ Assets/Scripts/Player/Movement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+
+    }
+
+    private void Jump()
+    {
+
+    }
+
+    private void Throw()
+    {
+
+    }
+
+    private void Tag()
+    {
+
+    }
+}
