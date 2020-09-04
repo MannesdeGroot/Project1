8e8913c9bfe46767bc1e0cabe0c98fd53e78225e
AnthonieP <49709501+AnthonieP@users.noreply.github.com>
2020-09-04 20:26:32 +0200

    PhotonSetup

---
+++ Assets/_Anthonie/Code/Multiplayer/NetworkController.cs
@@ -0,0 +1,24 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkController : MonoBehaviourPunCallbacks
+{
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        base.OnConnectedToMaster();
+
+        print("Connected to " + PhotonNetwork.CloudRegion + " server.");
+    }
+}
