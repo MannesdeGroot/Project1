f7b61cdfe74de91d0aa173101657b77275d88b9c
AnthonieP <49709501+AnthonieP@users.noreply.github.com>
2020-09-16 16:10:39 +0200

    tag manager update

--- Assets/_Anthonie/Code/TagManager.cs
+++ Assets/_Anthonie/Code/TagManager.cs
@@ -3,11 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TagManager : MonoBehaviour, Photon.Pun.IPunObservable
+public class TagManager : MonoBehaviour
 {
     public float roundTime;
     public List<string> playerIDs = new List<string>();
     public PhotonView pV;
+    public string[] playerIDsArray;
     void Start()
     {
         pV = transform.GetComponent<PhotonView>();
@@ -17,48 +18,20 @@ public class TagManager : MonoBehaviour, Photon.Pun.IPunObservable
     {
         if (Input.GetKeyDown(KeyCode.C))
         {
-            UpdatePlayerIDs();
-            AppointNewTagger();
+            ChooseTagger();
         }
     }
 
-    public void AppointNewTagger()
-    {
-        pV.RPC("ChooseTagger", RpcTarget.All, playerIDs[Random.Range(0, playerIDs.Count - 1)]);
-    }
+    
 
-    [PunRPC]
-    public void ChooseTagger(string idOfPlayer)
+    
+    public void ChooseTagger()
     {
         PlayerController[] players = FindObjectsOfType<PlayerController>();
-        for (int i = 0; i < players.Length; i++)
-        {
-            if(players[i].pV.ViewID.ToString() == idOfPlayer)
-            {
-                players[i].PhotonTag(transform.position, 0);
-            }
-        }
+        players[Random.Range(0, players.Length - 1)].PhotonTag(transform.position, 0);
+
+
     }
 
-    public void UpdatePlayerIDs()
-    {
-        PlayerController[] players = FindObjectsOfType<PlayerController>();
-        playerIDs = new List<string>();
-        for (int i = 0; i < players.Length; i++)
-        {
-            playerIDs.Add(players[i].pV.ViewID.ToString());
-        }
-    }
-
-    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
-    {
-        if (stream.IsWriting)
-        {
-            stream.SendNext(playerIDs);
-        }
-        else if (stream.IsReading)
-        {
-            playerIDs = (List<string>)stream.ReceiveNext();
-        }
-    }
+    
 }
