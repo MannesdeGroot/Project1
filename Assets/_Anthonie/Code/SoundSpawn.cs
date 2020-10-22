f0c330b2b76c33c0ab26cb1cfe76892e3b10f77a
AnthonieP <49709501+AnthonieP@users.noreply.github.com>
2020-10-22 22:43:25 +0200

    sounds

---
+++ Assets/_Anthonie/Code/SoundSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundSpawn : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public float randomPitch;
+    public float destroyAfterTime;
+    void Start()
+    {
+        audioSource.pitch += Random.Range(-randomPitch, randomPitch);
+        audioSource.Play();
+    }
+
+    void Update()
+    {
+        destroyAfterTime -= Time.deltaTime;
+        if(destroyAfterTime < 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
